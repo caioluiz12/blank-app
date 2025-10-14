a998e7d40f98d0c5c63e58365c85407450a3c351
caioluiz12 <caio.reis@unifal-mg.edu.br>
2025-10-14 08:19:20 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ import re
 
 # Configurar chave da API Gemini
 genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
-model = genai.GenerativeModel("models/gemini-1.5-flash")
+model = genai.GenerativeModel("gemini-1.5-flash")
 
 # Função para extrair texto de uma página web
 def extrair_texto(url):
